64eb70e06de7ad23cc5805bf870f4616e35c9f3f
Mohammed Mahi <mohammed.mahi012@gmail.com>
2025-07-24 12:45:44 +0100

    feat: add axios for API requests, implement async login with error handling in auth slice, and enhance Login component with alerts

--- src/modules/auth/authSlice.ts
+++ src/modules/auth/authSlice.ts
@@ -1,5 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { FormType } from "./types";
+import { API_AXIOS } from "@/api/API_AXIOS";
+
+
+export const asyncLogin = createAsyncThunk("auth/login", async (formData: FormType,thunkAPI) => {
+    const {rejectWithValue} = thunkAPI
+    try {
+        const res = await API_AXIOS.post("user/login",formData)
+        console.log(res);
+        
+    } catch (error:any) {
+        return rejectWithValue(error.response)
+        
+    }
+});
 
 
 
@@ -15,7 +29,26 @@ error: null
 const authSlice = createSlice({
     name:"auth",
     initialState,
-    reducers:{}
+    reducers:{},
+    extraReducers: (builder) => {
+        // Login
+        builder.addCase(asyncLogin.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+        builder.addCase(asyncLogin.fulfilled, (state, {payload}) => {
+            state.isLoading = false;
+
+        });
+        builder.addCase(asyncLogin.rejected, (state, {payload}:PayloadAction<any>) => {
+            state.isLoading = false;
+            if (payload) {
+                state.error = payload.data;
+            } else {
+                state.error = "An error occurred during login.";
+            }
+        });
+    }
 })
 
 export default authSlice.reducer;
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,43 +1,67 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import Link from '@/components/ui/link'
-import Spinner from '@/components/ui/spinner'
-import { useAppSelector } from '@/hooks/storeHooks'
-import { useForm, type SubmitHandler } from "react-hook-form"
-import type { FormType } from '@/modules/auth/types'
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "@/components/ui/link";
+import Spinner from "@/components/ui/spinner";
+import { useAppDispatch, useAppSelector } from "@/hooks/storeHooks";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import type { FormType } from "@/modules/auth/types";
+import { asyncLogin } from "@/modules/auth/authSlice";
+import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const Login = () => {
-  const {isLoading} = useAppSelector(state => state.auth)
-
-    const {
+  const { isLoading, error } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+  const {
     register,
     setError,
     handleSubmit,
     watch,
-    formState: { errors},
-  } = useForm<FormType>()
-  const onSubmit: SubmitHandler<FormType> = (data) => data
-  console.log(errors.root?.message);
-  
+    formState: { errors },
+  } = useForm<FormType>();
+  const onSubmit: SubmitHandler<FormType> = (data) =>
+    dispatch(asyncLogin(data));
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 w-96 p-4 bg-white rounded-lg shadow-lg items-center'>
-        <h1 className='text-2xl font-semibold'>Get started with login</h1>
-        
-        <Input placeholder='Email' type='email' {...register("email",{ required:"Email is required" })} error={errors.email?.message} />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 w-96 p-4 bg-white rounded-lg shadow-lg items-center"
+      >
+        <h1 className="text-2xl font-semibold">Get started with login</h1>
+        {
+          error && (
+            <Alert variant="destructive">
+              <AlertCircleIcon />
+              <AlertTitle>{error}</AlertTitle>
+            </Alert>
+          )
+        }
+        <Input
+          placeholder="Email"
+          type="email"
+          {...register("email", { required: "Email is required" })}
+          error={errors.email?.message}
+        />
 
-        <Input placeholder='Password' type='password' {...register("password",{required:"Password is required"})} error={errors.password?.message}/>
+        <Input
+          placeholder="Password"
+          type="password"
+          {...register("password", { required: "Password is required" })}
+          error={errors.password?.message}
+        />
 
-        <Button type='submit' size={"lg"} className='w-full'>{isLoading ? <Spinner/>:"Login"}</Button>
+        <Button type="submit" size={"lg"} className="w-full">
+          {isLoading ? <Spinner /> : "Login"}
+        </Button>
 
-        <div className='flex gap-4 text-sm justify-between w-full'> 
-           <Link href="/reset-password">I forget password ?</Link>
-           <Link href="/register">I dont have account</Link>
+        <div className="flex gap-4 text-sm justify-between w-full">
+          <Link href="/reset-password">I forget password ?</Link>
+          <Link href="/register">I dont have account</Link>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

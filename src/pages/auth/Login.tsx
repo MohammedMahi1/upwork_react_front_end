22e363fb434aaca0cfd4e3070c2993cf97472750
Mohammed Mahi <mohammed.mahi012@gmail.com>
2025-07-24 11:42:22 +0100

    feat: implement form handling with react-hook-form in Login component

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,15 +3,39 @@ import { Input } from '@/components/ui/input'
 import Link from '@/components/ui/link'
 import Spinner from '@/components/ui/spinner'
 import { useAppSelector } from '@/hooks/storeHooks'
+import { useForm, type SubmitHandler } from "react-hook-form"
+import type { FormType } from '@/modules/auth/types'
+
+
 const Login = () => {
   const {isLoading} = useAppSelector(state => state.auth)
+
+    const {
+    register,
+    setError,
+    handleSubmit,
+    watch,
+    
+    formState: { errors},
+  } = useForm<FormType>()
+  const onSubmit: SubmitHandler<FormType> = (data) => data
+  console.log(errors.root?.message);
+  
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <form onSubmit={()=>console.log("Submited")} className='flex flex-col gap-4 w-96 p-4 bg-white rounded-lg shadow-lg items-center'>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 w-96 p-4 bg-white rounded-lg shadow-lg items-center'>
         <h1 className='text-2xl font-semibold'>Get started with login</h1>
-        <Input placeholder='Email' type='email'/>
-        <Input placeholder='Password' type='password'/>
-        <Button size={"lg"} className='w-full'>{isLoading ? <Spinner/>:"Login"}</Button>
+
+
+        <span className='text-red-400'>{errors.email?.message}</span>
+        <Input placeholder='Email' type='email' {...register("email",{ required:"Email is required" })} />
+
+
+        <span className='text-red-400'>{errors.password?.message}</span>
+        <Input placeholder='Password' type='password' {...register("password",{required:"Password is required"})}/>
+
+
+        <Button type='submit' size={"lg"} className='w-full'>{isLoading ? <Spinner/>:"Login"}</Button>
 
         <div className='flex gap-4 text-sm justify-between w-full'> 
            <Link href="/reset-password">I forget password ?</Link>
